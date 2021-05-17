97e835c763a81187fd18bdae6a29777991881d50
Ernesto Diaz <ernesto.adp1@gmail.com>
2021-05-16 23:37:24 -0400

    custom skill y contacto

--- src/App.vue
+++ src/App.vue
@@ -28,19 +28,29 @@
     </div>
 
     <router-view></router-view>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-      felis mauris, tempus condimentum lacus ut, luctus ullamcorper justo. Donec
-      vel rhoncus mauris. Proin vestibulum consequat felis vitae porta. Aenean
-      est sapien, porttitor vestibulum felis eu, convallis euismod turpis. Nunc
-      vitae tellus felis. Cras quis sodales dolor. Suspendisse vulputate vitae
-      tellus felis. Cras quis sodales dolor. Suspendisse vulputate ultricies
-      tortor, ac sollicitudin dolor commodo vitae. Nam sodales vestibulum
-      feugiat. Nullam tincidunt eleifend imperdiet. Integer vulputate, turpis
-      non blandit fermentum, diam lectus dignissim lorem, sit amet lacinia sem
-      ligula quis ex. Praesent condimentum erat in nulla dictum, sit amet rutrum
-      magna commodo. Etiam nec nisl sit amet nulla pellentesque mattis.
-    </p>
+    <div class="contacto">
+      <h3>CONTACTO</h3>
+
+      <div class="container">
+        <form action="/action_page.php">
+          <label for="fname">Nombre</label>
+          <input type="text" id="fname" name="firstname" placeholder="Nombre" />
+
+          <label for="lname">Asunto</label>
+          <input type="text" id="lname" name="lastname" placeholder="Asunto" />
+
+          <label for="subject">Mensaje</label>
+          <textarea
+            id="subject"
+            name="subject"
+            placeholder="Mensaje.."
+            style="height: 200px"
+          ></textarea>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -96,7 +106,7 @@ body {
   background-image: url("./assets/2.jpg");
   background-size: cover;
   background-position: center;
-  opacity: 0.5;
+  opacity: 0.9;
   top: 0;
   left: 0;
   bottom: 0;
@@ -120,7 +130,7 @@ body {
   transform: translate(-50%, -50%);
 }
 
-/** bototones de skill/proyecto/hobbie */
+/** ---------------------------------------bototones de skill/proyecto/hobbie ----------------------------------*/
 .boton-uno,
 .boton-tres {
   width: 9%;
@@ -147,39 +157,111 @@ a:active {
   color: white;
 }
 
+/**------------------------------------------------------fin------------------------------------------------------------------------- */
+
 p {
   font-family: "Roboto", sans-serif;
   width: 60%;
   margin: auto;
   font-weight: 100;
   color: #000000;
+  /**  animate__bounceInRight*/
+}
+/**----------------------------barras de skills---------------------------------------------------- */
+.boss {
+  background-image: url("./assets/8.jpg");
+
+  width: 100%;
+  background-size: cover;
+}
+.contenedor-padre {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  padding-bottom: 100px;
 }
 
 .w3-container {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
   width: 100%;
-  background-image: url("./assets/4.jpg");
-  background-size: cover;
-
-  opacity: 0.7;
-
-  border: 4px solid rgb(0, 0, 0);
- 
 }
 
 .w3-light-grey {
-  margin: auto;
-  width: 50%;
-  border: 4px solid rgb(0, 0, 0);
+  background-color: #ffffff9f !important;
+  width: 80%;
+  border: 3px solid rgb(0, 0, 0);
   border-radius: 4px;
 }
 
-.p-skills{
+.w3-green {
+  background-color: #000000fd !important;
+}
+
+.skills {
   color: white;
 }
-.p-skills-dos{
-  color: rgb(0, 0, 0);
+.p-skills {
+  color: white;
+  font-weight: bolder;
 }
+.p-skills-dos {
+  color: rgb(0, 0, 0);
+  font-weight: bolder;
+}
+/**-------------------------------------------------------fin----------------------------------- */
+
+/**--------------------------------------------------------CONTACTO----------------------------------- */
+.contacto {
+  width: 60%;
+  padding-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+
+  margin: auto;
+}
+
+input[type="text"],
+select,
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type="submit"] {
+  margin: 3px;
+  background-color: #31313100;
+  color: rgb(0, 0, 0);
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin-top: 40px;
+  margin-bottom: 20px;
+  border: 1px solid rgb(0, 0, 0);
+  border-radius: 4px;
+}
+
+input[type="submit"]:hover {
+  background-color: #000000;
+  color: white;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: rgb(211, 211, 211);
+  border: 3px solid rgb(0, 0, 0);
+  padding: 20px;
+}
+
+/**------------------------------------------------FIN---------------------------------------------------------- */
 </style>
--- src/components/Skils.vue
+++ src/components/Skils.vue
@@ -1,94 +1,61 @@
 <template>
-  <div class="">
+  <div class="boss">
     <h2 class="skills">SKILLS</h2>
-    
-    <div class="w3-container">
-      
-      <p class="p-skills">CSS3</p>
-      <div class="w3-light-grey">
-        <div
-          id="css"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
+    <div class="contenedor-padre">
+      <div class="w3-container">
+        <p class="p-skills">CSS3</p>
+
+        <div class="w3-light-grey">
+          <div id="css" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
+
+        <p class="p-skills">HTML5</p>
+        <div class="w3-light-grey">
+          <div id="html" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
+
+        <p class="p-skills">BOOSTRAP4</p>
+        <div class="w3-light-grey">
+          <div id="boostrap" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
+
+        <p class="p-skills">JAVASCRIPT</p>
+        <div class="w3-light-grey">
+          <div
+            id="javaSCript"
+            class="w3-green w3-center"
+            style="width: 0%"
+          ></div>
+        </div>
+        <p class="p-skills">VUE.JS</p>
+        <div class="w3-light-grey">
+          <div id="Vuejs" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
       </div>
 
-      <p class="p-skills">HTML5</p>
-      <div class="w3-light-grey">
-        <div
-          id="html"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
+      <div class="w3-container">
+        <p class="p-skills">PHP</p>
+        <div class="w3-light-grey">
+          <div id="php" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
 
-      <p class="p-skills">BOOSTRAP 4</p>
-      <div class="w3-light-grey">
-        <div
-          id="boostrap"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
+        <p class="p-skills">GIT</p>
+        <div class="w3-light-grey">
+          <div id="git" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
 
-      <p class="p-skills">JAVASCRIPT</p>
-      <div class="w3-light-grey">
-        <div
-          id="javaSCript"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
+        <p class="p-skills">JAVA</p>
+        <div class="w3-light-grey">
+          <div id="java" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
 
-      <p class="p-skills">VUE.JS</p>
-      <div class="w3-light-grey">
-        <div
-          id="Vuejs"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
+        <p class="p-skills">MVC & CRUD</p>
+        <div class="w3-light-grey">
+          <div id="mvc" class="w3-green w3-center" style="width: 0%"></div>
+        </div>
       </div>
-
-      <p class="p-skills">PHP</p>
-      <div class="w3-light-grey">
-        <div
-          id="php"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
-
-      <p class="p-skills">GIT</p>
-      <div class="w3-light-grey">
-        <div
-          id="git"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
-
-      <p class="p-skills-dos">JAVA</p>
-      <div class="w3-light-grey">
-        <div
-          id="java"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
-
-      <p class="p-skills-dos">MVC & CRUD</p>
-      <div class="w3-light-grey">
-        <div
-          id="mvc"
-          class="w3-container w3-green w3-center"
-          style="width: 0%"
-        ></div>
-      </div>
-      <br>
-     
     </div>
-    </div>
-  
+  </div>
 </template>
 
 <script>
